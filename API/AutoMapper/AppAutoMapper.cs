c7fb3ce72b6ce332bd443058958ff0cb196eaf5c
Theophilus Unekwu <theophilusu@examroom.ai>
2024-05-15 18:27:53 +0100

    added AutoMapper class

---
+++ API/AutoMapper/AppAutoMapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Data.Dtos;
+using Data.Models;
+
+namespace API.AutoMapper
+{
+    public class AppAutoMapper : Profile
+    {
+        public AppAutoMapper()
+        {
+            CreateMap<AddQuestionTypeDto, QuestionType>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString())).ReverseMap();
+
+            CreateMap<AddQuestionDto, Question>()
+           .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString())).ReverseMap();
+
+            CreateMap<UpdateQuestionDto, Question>().ReverseMap();
+
+            CreateMap<ApplicationDto, Application>()
+          .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString())).ReverseMap();
+
+            CreateMap<PersonalInformationDto, PersonalInformation>().ReverseMap();
+
+            CreateMap<AdditionalQuestionDto, AdditionalQuestion>().ReverseMap();
+
+            CreateMap<ViewApplicationDto, Application>().ReverseMap();
+        }
+    }
+}
